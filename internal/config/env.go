726a21a549cd7bec24504feb072c02b3066f9f2c
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2023-12-13 21:25:11 +0545

    chore: add validation to ensure JWT_SECRET is present as env variable

--- internal/config/env.go
+++ internal/config/env.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -10,5 +11,15 @@ func LoadEnv() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	// Check if the required environment variables are set
+	checkEnv("JWT_SECRET")
+}
+
+func checkEnv(key string) {
+	_, exists := os.LookupEnv(key)
+	if !exists {
+		log.Fatalf("Environment variable %s is not set", key)
 	}
 }
